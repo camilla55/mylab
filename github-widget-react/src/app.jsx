d9d9375153c56fa76cde53781d514fbf6ab0648e
cff20@sina.com <cff20@sina.com>
2016-08-31 12:23:13 +0800

    remove

--- github-widget-react/src/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {render} from 'react-dom';
-import GithubWedgit from './widget.jsx';
-
-render(
-    <GithubWedgit name="kyo4311"></GithubWedgit>,
-    document.getElementById('example')
-)
--- github-widget-vue/index.js
+++
@@ -1,9 +0,0 @@
-var GithubWidgetComponent = require('./components/widget.js');
-
-// 注册
-Vue.component('githubwidget', GithubWidgetComponent);
-
-// 创建根实例
-new Vue({
-    el: '#example'
-});
